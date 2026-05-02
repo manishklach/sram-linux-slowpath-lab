ca45c5c6583cd54801b8decfe4f82eae7dc43ba4
Manish KL <manishklach@gmail.com>
2026-05-02 11:45:11 -0700

    Replace sleep with deterministic busy-wait for device execution

--- benchmarks/fixed_buffers_sim.c
+++ benchmarks/fixed_buffers_sim.c
@@ -3,25 +3,33 @@
 #include <time.h>
 #include <unistd.h>
 
-long get_ns() {
+#include <stdint.h>
+
+static inline uint64_t now_ns(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-    return ts.tv_sec * 1000000000L + ts.tv_nsec;
+    return ((uint64_t) ts.tv_sec * 1000000000ull) + (uint64_t) ts.tv_nsec;
+}
+
+static inline void busy_wait_ns(uint64_t target_ns) {
+    uint64_t start = now_ns();
+    while ((now_ns() - start) < target_ns) {
+#if defined(__x86_64__) || defined(__i386__)
+        __asm__ volatile("pause");
+#else
+        __asm__ volatile("" ::: "memory");
+#endif
+    }
 }
 
 void simulate_sram_execution() {
     // 20 microseconds
-    long start = get_ns();
-    while (get_ns() - start < 20000) {
-        // busy wait
-    }
+    busy_wait_ns(20000);
 }
 
 void simulate_memory_setup() {
-    long start = get_ns();
-    while (get_ns() - start < 5000) {
-        // busy wait 5us for memory setup
-    }
+    // 5us for memory setup
+    busy_wait_ns(5000);
 }
 
 int main(int argc, char *argv[]) {
@@ -34,24 +42,23 @@ int main(int argc, char *argv[]) {
     simulate_memory_setup();
 
     for (int i = 1; i <= iterations; i++) {
-        long t0 = get_ns();
+        uint64_t t0 = now_ns();
         
-        long t1 = get_ns(); // No setup inside loop
+        uint64_t t1 = now_ns(); // No setup inside loop
         
         simulate_sram_execution();
-        long t2 = get_ns();
+        uint64_t t2 = now_ns();
         
         // Simulating completion overhead
-        long start_completion = get_ns();
-        while (get_ns() - start_completion < 3000) {} // 3us completion
-        long t3 = get_ns();
+        busy_wait_ns(3000);
+        uint64_t t3 = now_ns();
         
-        long submit_ns = t1 - t0;
-        long device_ns = t2 - t1;
-        long completion_ns = t3 - t2;
-        long total_ns = t3 - t0;
+        uint64_t submit_ns = t1 - t0;
+        uint64_t device_ns = t2 - t1;
+        uint64_t completion_ns = t3 - t2;
+        uint64_t total_ns = t3 - t0;
         
-        printf("{\"iter\":%d,\"submit_ns\":%ld,\"device_ns\":%ld,\"completion_ns\":%ld,\"total_ns\":%ld}\n",
+        printf("{\"iter\":%d,\"submit_ns\":%lu,\"device_ns\":%lu,\"completion_ns\":%lu,\"total_ns\":%lu}\n",
                i, submit_ns, device_ns, completion_ns, total_ns);
     }
     return 0;
--- benchmarks/polling_sim.c
+++ benchmarks/polling_sim.c
@@ -3,17 +3,27 @@
 #include <time.h>
 #include <unistd.h>
 
-long get_ns() {
+#include <stdint.h>
+
+static inline uint64_t now_ns(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-    return ts.tv_sec * 1000000000L + ts.tv_nsec;
+    return ((uint64_t) ts.tv_sec * 1000000000ull) + (uint64_t) ts.tv_nsec;
+}
+
+static inline void busy_wait_ns(uint64_t target_ns) {
+    uint64_t start = now_ns();
+    while ((now_ns() - start) < target_ns) {
+#if defined(__x86_64__) || defined(__i386__)
+        __asm__ volatile("pause");
+#else
+        __asm__ volatile("" ::: "memory");
+#endif
+    }
 }
 
 void simulate_sram_execution() {
-    long start = get_ns();
-    while (get_ns() - start < 20000) {
-        // busy wait
-    }
+    busy_wait_ns(20000);
 }
 
 int main(int argc, char *argv[]) {
@@ -27,38 +37,35 @@ int main(int argc, char *argv[]) {
     }
 
     // Setup outside loop
-    long start_mem = get_ns();
-    while(get_ns() - start_mem < 5000) {}
+    busy_wait_ns(5000);
 
     srand(time(NULL));
 
     for (int i = 1; i <= iterations; i++) {
-        long t0 = get_ns();
-        long t1 = get_ns(); // fixed buffer simulation
+        uint64_t t0 = now_ns();
+        uint64_t t1 = now_ns(); // fixed buffer simulation
         
         simulate_sram_execution();
-        long t2 = get_ns();
+        uint64_t t2 = now_ns();
         
-        long t3;
+        uint64_t t3;
         if (polling_mode) {
             // Polling: very fast completion, deterministic
-            long start_completion = get_ns();
-            while (get_ns() - start_completion < 1000) {} // 1us polling overhead
-            t3 = get_ns();
+            busy_wait_ns(1000); // 1us polling overhead
+            t3 = now_ns();
         } else {
             // Interrupt: variable delay (context switch, softirq overhead)
-            long start_completion = get_ns();
             long delay = 3000 + (rand() % 5000); // 3us to 8us variable delay
-            while (get_ns() - start_completion < delay) {}
-            t3 = get_ns();
+            busy_wait_ns(delay);
+            t3 = now_ns();
         }
         
-        long submit_ns = t1 - t0;
-        long device_ns = t2 - t1;
-        long completion_ns = t3 - t2;
-        long total_ns = t3 - t0;
+        uint64_t submit_ns = t1 - t0;
+        uint64_t device_ns = t2 - t1;
+        uint64_t completion_ns = t3 - t2;
+        uint64_t total_ns = t3 - t0;
         
-        printf("{\"iter\":%d,\"submit_ns\":%ld,\"device_ns\":%ld,\"completion_ns\":%ld,\"total_ns\":%ld}\n",
+        printf("{\"iter\":%d,\"submit_ns\":%lu,\"device_ns\":%lu,\"completion_ns\":%lu,\"total_ns\":%lu}\n",
                i, submit_ns, device_ns, completion_ns, total_ns);
     }
     return 0;
